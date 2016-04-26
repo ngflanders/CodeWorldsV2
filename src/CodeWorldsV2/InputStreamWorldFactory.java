641e81e11c49d5a38573397eef839ffea9993918
Nicholas <ngflanders@gmail.com>
2016-04-26 00:11:08 -0500

    Added File Input and InputStreamWorldFactory

---
+++ src/CodeWorldsV2/InputStreamWorldFactory.java
@@ -0,0 +1,41 @@
+package CodeWorldsV2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class InputStreamWorldFactory implements WorldFactory {
+
+    public InputStreamWorldFactory(InputStream inputStream) {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder out = new StringBuilder();
+        String line;
+
+        try {
+
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            System.out.println(out.toString());   //Prints the string content read from input stream
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public Body getWorld() {
+        return null;
+    }
+
+    @Override
+    public WorldFactory build() throws CWSException {
+        return null;
+    }
+}
