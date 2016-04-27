eb5b6471dcc0651d233ebfc26061b01719da1871
Nicholas <ngflanders@gmail.com>
2016-04-27 11:47:10 -0500

    Added Horse Graphics

--- src/CodeWorldsV2/Horse.java
+++ src/CodeWorldsV2/Horse.java
@@ -25,13 +25,52 @@ public class Horse extends HerdAnimal {
     public Image getImage(int size) {
         Graphics2D grp;
         Color dkBrn = new Color(80, 60, 40);
+        Color bodyClr = new Color(210, 210, 210), hornClr = Color.DARK_GRAY,
+                hoofClr = new Color(80, 60, 40), spotClr = Color.BLACK;
 
         if (size != imgSize) {
             img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
             grp = img.createGraphics();
 
+            // Body and head
             grp.setColor(dkBrn);
-            grp.fillOval(3*size/10, 3*size/10, size/5, size/5);
+            grp.fillRoundRect(4*size/10, 4*size/10, 7*size/10, 5*size/10, size/10,
+                    size/10);
+
+            grp.fillRoundRect(size/10,  2*size/10,  3*size/10,  2*size/10,
+                    size/10, size/10);
+
+            // Legs
+            grp.fillPolygon(
+                    new int[] {size*4/10, size*5/10, size*4/10, size*5/20},
+                    new int[] {size/2, size/2, size*17/20, size*17/20}, 4);
+            grp.fillPolygon(
+                    new int[] {size*7/10, size*8/10, size*9/10, size*15/20},
+                    new int[] {size/2, size/2, size*17/20, size*17/20}, 4);
+
+            // Hooves
+            grp.setColor(hoofClr);
+            grp.fillPolygon(
+                    new int[] {size*5/20, size*4/10, size*9/20, size*2/10},
+                    new int[] {size*17/20, size*17/20, size*19/20, size*19/20}, 4);
+            grp.fillPolygon(
+                    new int[] {size*15/20, size*9/10, size*19/20, size*7/10},
+                    new int[] {size*17/20, size*17/20, size*19/20, size*19/20}, 4);
+
+            // Spots and eye
+            grp.setColor(spotClr);
+            grp.fillOval(4*size/10,  4*size/10,  size/10,  size/10);
+            grp.fillOval(6*size/10,  5*size/10,  size/5,  size/8);
+            grp.fillOval(3*size/10, 5*size/20, size/40, size/40);
+
+            // Horn
+            grp.setColor(hornClr);
+            grp.fillPolygon(
+                    new int[] {3*size/10, 7*size/20, 5*size/20},
+                    new int[] {2*size/10, 2*size/10, size/10}, 3);
+
+            Color greenish = new Color(230, 255, 230);
+            grp.fillOval(10,  20, 5, 5);
 
             grp.dispose();
         }
