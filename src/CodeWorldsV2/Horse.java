c441b19b4e24a98c634a3e7f953bfcf05c34ee54
Martin Soemarsono <martinsoemarsono@hotmail.com>
2016-05-01 14:46:49 -0500

    Horse done!

--- src/CodeWorldsV2/Horse.java
+++ src/CodeWorldsV2/Horse.java
@@ -24,31 +24,26 @@ public class Horse extends HerdAnimal {
     @Override
     public Image getImage(int size) {
         Graphics2D grp;
-        Color bodyClr = new Color(100, 100, 100), hornClr = Color.DARK_GRAY,
+        Color bodyClr = new Color(169, 138, 61), hornClr = Color.DARK_GRAY,
                 hoofClr = new Color(60, 60, 40), spotClr = Color.BLACK;
 
         if (size != imgSize) {
             img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
             grp = img.createGraphics();
 
-            // Body and head
+            // Body, head, and ear
             grp.setColor(bodyClr);
-            grp.fillRoundRect(/*X location*/3*size/10, /*Y location*/3*size/10, /*Width of Rect*/ 6*size/10,
-                    /*Height of Rect*/ 4*size/10, size/10, size/10);
-            grp.fillRoundRect(size/10,  2*size/10,  3*size/10,  2*size/10,
+            grp.fillRoundRect(/*X location*/3*size/10, /*Y location*/4*size/10, /*Width of Rect*/ 5*size/10,
+                    /*Height of Rect*/ 2*size/10, size/10, size/10);
+            grp.fillRoundRect(size/20,  2*size/10,  3*size/10,  2*size/10,
                     size/10, size/10);
+            grp.fillPolygon(
+                    new int[] {5*size/20, 8*size/20, 9*size/20},
+                    new int[] {4*size/20, 4*size/20, 3*size/20}, 3);
 
-            // Hair
-            /**
-            grp.fillPolygon(
-                    new int[] {size*4/10, size*5/10, size*4/10, size*5/20},
-                    new int[] {size/2, size/2, size*17/20, size*17/20}, 4);
-            grp.fillPolygon(
-                    new int[] {size*7/10, size*8/10, size*9/10, size*15/20},
-                    new int[] {size/2, size/2, size*17/20, size*17/20}, 4);
-             **/
 
             // Legs
+
             grp.fillPolygon(
                     new int[] {size*4/10, size*5/10, size*4/10, size*5/20},
                     new int[] {size/2, size/2, size*17/20, size*17/20}, 4);
@@ -65,19 +60,35 @@ public class Horse extends HerdAnimal {
                     new int[] {size*15/20, size*9/10, size*19/20, size*7/10},
                     new int[] {size*17/20, size*17/20, size*19/20, size*19/20}, 4);
 
+            //Neck
+            grp.setColor(bodyClr);
+            grp.fillPolygon(
+                    new int[] {size*6/20, size*4/10, size*9/20, size*3/10},
+                    new int[] {size*4/20, size*4/20, size*9/20, size*9/20}, 4);
+
+            //Tail
+            grp.setColor(hornClr);
+            grp.fillPolygon(
+                    new int[] {16*size/20, 16*size/20, 19*size/20},
+                    new int[] {4*size/10, 5*size/10, 6*size/10}, 3);
+
             // eye
             grp.setColor(spotClr);
             grp.fillOval(2*size/10,  2*size/10,  size/15,  size/15);
 
 
-            // Horn
+            // Mane
             grp.setColor(hornClr);
             grp.fillPolygon(
-                    new int[] {3*size/10, 7*size/20, 5*size/20},
-                    new int[] {2*size/10, 2*size/10, size/10}, 3);
+                    new int[] {/*top right*/size*6/20, /*top left*/size*4/10, /*bottom right*/size*10/20,
+                            /*bottom left*/size*4/10},
+                    new int[] {/*top right*/size*4/20, /*top left*/size*4/20, /*bottom right*/size*8/20,
+                            /*bottom left*/size*8/20}, 4);
+            grp.fillPolygon(
+                    new int[] {size*6/20, size*4/10, size*9/20, size*4/10},
+                    new int[] {size*4/20, size*4/20, size*9/20, size*9/20}, 4);
 
             Color greenish = new Color(230, 255, 230);
-            grp.fillOval(10,  20, 5, 5);
 
             grp.dispose();
         }
